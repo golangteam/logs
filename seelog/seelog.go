ac6f9a971fc478036dcec9367286a32848dc82e5
zhangnaiguo <zhangnaiguo@1tu1shu.com>
2020-01-30 17:58:36 +0800

    init

---
+++ seelog/seelog.go
@@ -0,0 +1,56 @@
+package seelog
+
+import (
+	"github.com/cihub/seelog"
+	"github.com/golangteam/function/file"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	logConfig = `<seelog type="asynctimer" asyncinterval="5000000" minlevel="debug">
+	<outputs formatid="main">
+		<console/>
+		<rollingfile type="size" filename="__log_url__" maxsize="1024000" maxrolls="10" />
+	</outputs>
+	<formats>
+		<format id="main" format="%Date(2006-01-02 15:04:05) [%Level] %RelFile line:%Line %Msg%n"/>
+	</formats>
+</seelog>`
+)
+
+//Init init seelog
+//
+// @param pathLog    string  path of logs
+// @param pathConfig string  path of log.xml
+func Init(logPath, configPath string) {
+	
+	if logPath == "" {
+		logPath = "logs/roll.log"
+	}
+	tmp := filepath.Dir(logPath)
+	if file.FileIsNotExist(tmp) {
+		os.MkdirAll(tmp, 0764)
+	}
+	
+	if configPath == "" {
+		configPath =  "log.xml"
+	}
+	tmp = filepath.Dir(configPath)
+	if file.FileIsNotExist(tmp) {
+		os.MkdirAll(tmp, 0764)
+	}
+	if file.FileIsNotExist(configPath) {
+		ioutil.WriteFile(configPath, []byte(strings.Replace(logConfig, "__log_url__", logPath, 1)), 0764)
+	}
+	if logger, err := seelog.LoggerFromConfigAsFile(configPath); err == nil {
+		seelog.ReplaceLogger(logger)
+	}
+}
+
+//Flush flush the seelog
+func Flush() {
+	seelog.Flush()
+}
